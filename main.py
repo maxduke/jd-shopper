ef767d9a27eed8fbd14501c7a9d621d0f0c715c2
louisyoungx <1462648167@qq.com>
2022-03-19 00:15:47 +0800

    feat(main): function 0 - start server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Config import config
 from Core.spider import Waiter
+from runserver import running
 
 if __name__ == '__main__':
     choiceList = """  
@@ -7,14 +8,18 @@ if __name__ == '__main__':
 使用前请按要求填写config.ini中的信息
 
 功能列表：                                                                                
- 1.自动加购物车，缺货等待上架自动下单
- 2.自动定时加购物车下单（普通商品，非秒杀抢购）
+ 0. 打开面板（选择后使用浏览器打开 http://localhost:12021/）
+ 1. 自动加购物车，缺货等待上架自动下单
+ 2. 自动定时加购物车下单（普通商品，非秒杀抢购）
 """
     print(choiceList)
     choice_function = ''
     if choice_function == '':
-        choice_function = input('请选择:')
-    if choice_function == '1':
+        choice_function = input('请选择(输入数字):')
+    if choice_function == '0':
+        print("使用浏览器打开 http://localhost:12021/")
+        running()
+    elif choice_function == '1':
         waiter = Waiter()
         waiter.waitForSell()
     elif choice_function == '2':

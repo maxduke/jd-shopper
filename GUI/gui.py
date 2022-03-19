a333151c7f028d7750c02341fb36d3fd0358b008
louisyoungx <1462648167@qq.com>
2022-03-20 05:47:04 +0800

    fix(gui): cannot import gui

--- GUI/gui.py
+++ GUI/gui.py
@@ -6,22 +6,22 @@ PROJECT = config.Information.project
 SERVER_HOST = config.Server.server_host
 PORT = config.Server.port
 
-# def gui():
-#     url = "http://{}:{}/".format(SERVER_HOST, PORT)
+def gui():
+    url = "http://{}:{}/".format(SERVER_HOST, PORT)
 
-#     def on_closed():
-#         os.kill(os.getpid(), signal.SIGTERM)
+    # def on_closed():
+    #     os.kill(os.getpid(), signal.SIGTERM)
 
-#     import webview
-#     window = webview.create_window(PROJECT,
-#                           url=url,
-#                           js_api=None,
-#                           width=900,
-#                           height=800,
-#                           resizable=True,
-#                           fullscreen=False,
-#                           min_size=(200, 200),
-#                           background_color='#FFF',
-#                           text_select=False)
-#     window.events.closed += on_closed
-#     webview.start()
+    # import webview
+    # window = webview.create_window(PROJECT,
+    #                       url=url,
+    #                       js_api=None,
+    #                       width=900,
+    #                       height=800,
+    #                       resizable=True,
+    #                       fullscreen=False,
+    #                       min_size=(200, 200),
+    #                       background_color='#FFF',
+    #                       text_select=False)
+    # window.events.closed += on_closed
+    # webview.start()

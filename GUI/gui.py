5782c12163effbab923ae98794bf6896d84411da
louisyoungx <1462648167@qq.com>
2022-03-20 05:32:37 +0800

    feat(gui): close window & kill self process

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,23 +1,27 @@
-
-
+import os
+import signal
 from Config import config
 
 PROJECT = config.Information.project
 SERVER_HOST = config.Server.server_host
 PORT = config.Server.port
 
-def gui():
-    url = "http://{}:{}/".format(SERVER_HOST, PORT)
+# def gui():
+#     url = "http://{}:{}/".format(SERVER_HOST, PORT)
 
-    # import webview
-    # webview.create_window(PROJECT,
-    #                       url=url,
-    #                       js_api=None,
-    #                       width=900,
-    #                       height=800,
-    #                       resizable=True,
-    #                       fullscreen=False,
-    #                       min_size=(200, 200),
-    #                       background_color='#FFF',
-    #                       text_select=False)
-    # webview.start()
+#     def on_closed():
+#         os.kill(os.getpid(), signal.SIGTERM)
+
+#     import webview
+#     window = webview.create_window(PROJECT,
+#                           url=url,
+#                           js_api=None,
+#                           width=900,
+#                           height=800,
+#                           resizable=True,
+#                           fullscreen=False,
+#                           min_size=(200, 200),
+#                           background_color='#FFF',
+#                           text_select=False)
+#     window.events.closed += on_closed
+#     webview.start()
